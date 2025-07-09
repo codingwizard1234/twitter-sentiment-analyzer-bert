f0c0ba4624f48ebe5f4fa4925229681fbd97153f
codingwizard1234 <142437805+codingwizard1234@users.noreply.github.com>
2025-07-09 11:33:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import zipfile
 
 # Define paths
 zip_path = "bert_model.zip"
-model_dir = "/Users/arnavkumargupta/Documents/programs/mlproject/bert_sentiment_model"
+model_dir = "bert_sentiment_model"
 
 # Download zip from Google Drive (replace FILE_ID)
 if not os.path.exists(zip_path):
-    url = "https://drive.google.com/file/d/1ThCDU0_i-s308kYueOTiuWfCti4NTl_E/view?usp=drive_link"
+    url = "https://drive.google.com/uc?id=1ThCDU0_i-s308kYueOTiuWfCti4NTl_E"
     gdown.download(url, zip_path, quiet=False)
 
 # Unzip if model folder doesn't exist
